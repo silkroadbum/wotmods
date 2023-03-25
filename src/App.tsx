6703680ac13d1bbd7be2b91d2f7c1d448dde05aa
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-03-25 23:21:57 +0600

    Добавил страницу 404

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from './Layout/Layout';
 import FullNews from './screens/FullNews';
 import Mods from './screens/Mods';
 import News from './screens/News';
+import NotFound from './screens/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Mods />} />
           <Route path="/news" element={<News />} />
           <Route path="/news/:id" element={<FullNews />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
     </>
---
+++ src/screens/NotFound.tsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <section className="error">
+      <h1 className="title">Error 404</h1>
+      <p>Страница не найдена</p>
+    </section>
+  );
+}
+
+export default NotFound;
