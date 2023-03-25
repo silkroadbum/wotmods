cf6bd84805fd3eec247e244d17e843137bd33bd1
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-03-25 22:47:08 +0600

    Правка стилей для отступов основного содержимого

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,22 +1,13 @@
-import { MENU_ITEM, MENU_LINK } from '../../const';
-import { Link } from 'react-router-dom';
 import './Footer.scss';
 import SocialList from '../SocialList/SocialList';
+import NavList from '../NavList/NavList';
 
 function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="main-footer">
       <div className="container container--footer">
-        <ul className="main-footer__list">
-          {MENU_ITEM.map((item, i) => (
-            <li key={i} className="main-footer__item">
-              <Link to={MENU_LINK[i]} className="main-footer__link">
-                {item}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <NavList />
         <p className="copyright">Анатолий Егоров &copy; {year}</p>
         <SocialList />
       </div>
