56863f11b93c681d243ab9bff9f0b5ea9d22590f
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-03-25 22:43:36 +0600

    Поправил стили для навигации

--- src/components/NavList/NavList.tsx
+++ src/components/NavList/NavList.tsx
@@ -1,20 +1,14 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { MENU_ITEM, MENU_LINK } from '../../const';
 import './NavList.scss';
 
 function NavList() {
-  const [activeItem, setActiveItem] = useState(0);
-
   return (
     <ul className="nav-list">
       {MENU_ITEM.map((item, i) => (
         <li key={i} className="nav-list__item">
-          <Link
-            onClick={() => setActiveItem(i)}
-            to={MENU_LINK[i]}
-            className={`nav-list__link ${activeItem === i ? 'nav-list__link--active' : ''}`}>
+          <Link to={MENU_LINK[i]} className="nav-list__link">
             {item}
           </Link>
         </li>
