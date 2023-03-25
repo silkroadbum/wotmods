773f42ce90832fe9a4b7ae2f230b64f60b46b660
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-03-25 23:12:58 +0600

    Реализовал отрисовку только 6 новостей и подрузку следующих 6 новостей по кнопек

--- src/components/NewsList/NewsList.tsx
+++ src/components/NewsList/NewsList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../../store/store';
 
@@ -5,15 +6,25 @@ import NewsCard from '../NewsCard/NewsCard';
 import './NewsList.scss';
 
 function NewsList() {
+  const [showItemsCount, setShowItemsCount] = useState(6);
   const { news } = useSelector((state: RootState) => state.news);
+
+  const onClickShowMore = () => {
+    setShowItemsCount((prev) => prev + 6);
+  };
 
   return (
     <section className="news">
       <ul className="news__list">
-        {news.map((item) => (
+        {news.slice(0, showItemsCount).map((item) => (
           <NewsCard key={item.id} {...item} />
         ))}
       </ul>
+      {showItemsCount < news.length && (
+        <button className="btn btn--show-more" onClick={onClickShowMore}>
+          Показать еще
+        </button>
+      )}
     </section>
   );
 }
