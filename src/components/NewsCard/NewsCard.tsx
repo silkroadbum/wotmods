9ec394629ef23ca81139d2fc87c8687b9ca0e6be
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-03-25 21:08:50 +0600

    Стилизовал карточку новости

--- src/components/NewsCard/NewsCard.tsx
+++ src/components/NewsCard/NewsCard.tsx
@@ -3,16 +3,24 @@ import './NewsCard.scss';
 type NewsCardProps = {
   title: string;
   imgUrl: string;
+  date: string;
 };
 
-function NewsCard({ title, imgUrl }: NewsCardProps) {
+function NewsCard({ title, imgUrl, date }: NewsCardProps) {
   return (
     <li className="news__item">
-      <img className="news__image" src={imgUrl} alt={title} />
-      <h2 className="news__title">{title}</h2>
-      <button className="btn" type="button">
-        Подробнее
-      </button>
+      <a className="news__link" href="/">
+        <div className="news__preview">
+          <img className="news__image" src={imgUrl} alt={title} />
+          <h2 className="news__title">{title}</h2>
+        </div>
+      </a>
+      <footer className="news__footer">
+        <span className="news__date">{date}</span>
+        <button className="btn" type="button">
+          Подробнее
+        </button>
+      </footer>
     </li>
   );
 }
