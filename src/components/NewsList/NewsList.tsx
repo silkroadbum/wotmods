fa8e014dddeff18121b752f1b1d78aa6f3eb8958
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-03-26 17:31:07 +0600

    Рефакторинг

--- src/components/NewsList/NewsList.tsx
+++ src/components/NewsList/NewsList.tsx
@@ -1,28 +1,28 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../../store/store';
-
 import NewsCard from '../NewsCard/NewsCard';
 import './NewsList.scss';
 
 function NewsList() {
   const [showItemsCount, setShowItemsCount] = useState(6);
   const { news } = useSelector((state: RootState) => state.news);
-  console.log(news);
 
-  const onClickShowMore = () => {
-    setShowItemsCount((prev) => prev + 6);
+  const handleShowMore = () => {
+    setShowItemsCount((prevCount) => prevCount + 6);
   };
+
+  const newsToShow = news.slice(0, showItemsCount);
 
   return (
     <section className="news">
       <ul className="news__list">
-        {news.slice(0, showItemsCount).map((item) => (
+        {newsToShow.map((item) => (
           <NewsCard key={item.id} {...item} />
         ))}
       </ul>
       {showItemsCount < news.length && (
-        <button className="btn btn--show-more" onClick={onClickShowMore}>
+        <button className="btn btn--show-more" onClick={handleShowMore}>
           Показать еще
         </button>
       )}
