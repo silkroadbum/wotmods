ee626c2fa7cf2d0b39a3c84aca0729db03411457
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-03-26 19:57:03 +0600

    Добавил кнопку на страницу 404

--- src/screens/NotFound.tsx
+++ src/screens/NotFound.tsx
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function NotFound() {
   return (
     <section className="error">
       <h1 className="title">Error 404</h1>
-      <p>Страница не найдена</p>
+      <p className="error__description">Страница не найдена</p>
+      <Link className="btn btn--show-more" to="/">
+        Вернуться на главную
+      </Link>
     </section>
   );
 }
