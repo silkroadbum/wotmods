6cee996ad61399ac88e62f19605a127b37ac426e
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-03-26 22:59:17 +0600

    Добавил бургер меню на мобильных версий

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import NavList from '../NavList/NavList';
 import './Header.scss';
 
 function Header() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClickButton = () => {
+    setIsActive(!isActive);
+  };
   return (
     <header className="main-header">
       <div className="container container--header">
         <Link to="/" className="main-header__logo">
           WOTMODS
         </Link>
-        <nav className="nav">
+        <nav className={`nav ${isActive ? 'open' : ''}`}>
           <NavList />
         </nav>
+        <button onClick={handleClickButton} className={`burger-btn ${isActive ? 'active' : ''}`}>
+          <span></span>
+        </button>
       </div>
     </header>
   );
