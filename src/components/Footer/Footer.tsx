0f08afa230df733f3a7428eb7340eea83f5675a4
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-03-26 23:15:30 +0600

    Удалил навигацию из футера

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,11 @@
 import './Footer.scss';
 import SocialList from '../SocialList/SocialList';
-import NavList from '../NavList/NavList';
 
 function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="main-footer">
       <div className="container container--footer">
-        {/* <NavList /> */}
         <p className="copyright">Анатолий Егоров &copy; {year}</p>
         <SocialList />
       </div>
